f607b9a26c789286a26b86ffb0626c0a89754391
estiche <pepelaura@icloud.com>
2023-12-13 21:25:07 +0100

    css tarjetas

--- src/resultado.js
+++ src/resultado.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 
 export default function Resultado(props){
+  const tarjeta={
+    "border": "4px solid black",
+     "padding": "5px"
+  }
     
     function inicio() {
         return (
@@ -16,11 +20,11 @@ export default function Resultado(props){
         { props.productos? 
         <div>{props.productos.map((e,i)=>
           
-                <Card style={{ width: '18rem' , height : '20rem'}} key={i}>
-                <Link to={"/productos/" + i} ><Card.Img variant="top" src={e.thumbnail} /></Link>
+                <Card style={{ width: '18rem' , height : '20rem', border : '2px solid black', padding: '5px'}} key={i}>
+                <Link to={"/productos/" + i} ><Card.Img variant="top"  src={e.thumbnail} height="100"/></Link>
                   <Card.Body>
                     <Card.Title>{e.title}</Card.Title>
-                    <Card.Text>{e.description}</Card.Text>
+                    <Card.Text>{e.price}</Card.Text>
                   </Card.Body>
                 </Card>
            )}
