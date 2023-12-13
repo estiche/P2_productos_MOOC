0bfc770ac26e0570d1aacfce7198d4a21fe8d892
estiche <pepelaura@icloud.com>
2023-12-13 09:12:21 +0100

    empezando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import Tictactoe from './TicTacToe';
+import Peoducto from './Peoducto';
+import Home from './home';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Mis Juegos</h1>
+        <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
+          <Link to="/">Home</Link> |{' '}
+          <Link to="/producctos/peoductoid">Producto</Link> |{' '}
+        </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/producctos/peoductoid" element={<Producto/>} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
